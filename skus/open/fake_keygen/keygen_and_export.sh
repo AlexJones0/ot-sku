da74111729abe0b2bd320eb7b8334153283f17d1
Alex Jones <alex.jones@lowrisc.org>
2026-09-23 12:07:53 +0100

    [keygen] Small keygen script fixes and improvements
    - Change the bash shebang used for increased compatibility with
      different systems/environments.
    - Add some basic status prints to the keygen and export which describe
      which subdir we are generating keys for - this helps a bit with
      debugging misconfigured keygen & export templates.
    - Since the CA subdir currently doesn't export any keys in PEM format,
      the shell expansion of the `*.pem` globs will fail in the final tar.
      Since this is expected, set the shell option to not error on null
      globs whilst executing these tars to silence some noise.
    - The last time this script was run, only the CA key was generated - and
      the commenting out of other subdirs was kept in. Restore this to its
      original state.
    
    Signed-off-by: Alex Jones <alex.jones@lowrisc.org>

--- skus/open/fake_keygen/keygen_and_export.sh
+++ skus/open/fake_keygen/keygen_and_export.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Copyright lowRISC contributors (OpenTitan project).
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
@@ -13,15 +13,20 @@ DIR=$(dirname $0)
 
 function keygen_and_export {
   local dir=$1
+  echo "Generating keys for $1"
 
   cd ${dir}
   ${HSMTOOL} -t ${TOKEN} -u user -p ${PIN} exec keygen.json5
   ${HSMTOOL} -t ${TOKEN} -u user -p ${PIN} exec export.json5
   ${HSMTOOL} -t ${TOKEN} -u user -p ${PIN} exec export_public.json5
 
+  shopt -s nullglob
   tar -zcf ${dir}-private.tar.gz *.pem *.der
   tar -zcf ${dir}-public.tar.gz *.pub.pem *.pub.der
+  shopt -u nullglob
   cd ..
+
+  echo "Finished generating keys for $1"
 }
 
 # Change directory to the repository root
@@ -31,7 +36,7 @@ source skus/open/fake_keygen/softhsm_sourceme.sh
 
 # Now change to the keygen subdir and generate keys
 cd skus/open/fake_keygen
-#keygen_and_export root
-#keygen_and_export owner
-#keygen_and_export application
+keygen_and_export root
+keygen_and_export owner
+keygen_and_export application
 keygen_and_export ca
